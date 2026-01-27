505fac016032628e44e5c2f1a3617db333685264
tushar-behera15 <beheratushar013@gmail.com>
2026-01-27 22:41:56 +0530

    Enhance error handling in validateRequest middleware; use z.treeifyError for structured error responses

--- src/middleware/validate.middleware.ts
+++ src/middleware/validate.middleware.ts
@@ -1,11 +1,11 @@
 import { Request, Response, NextFunction } from "express";
-import { ZodSchema } from "zod";
+import z, { ZodSchema } from "zod";
 
 export const validateRequest = (schema: ZodSchema) => {
     return (req: Request, res: Response, next: NextFunction) => {
         const result = schema.safeParse(req.body);
         if (!result.success) {
-            return res.status(400).json({ errors: result.error });
+            return res.status(400).json({ errors: z.treeifyError(result.error) });
         }
         req.body = result.data;
         next();
