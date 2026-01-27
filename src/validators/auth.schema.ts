b5db5bb844b6b2a5ba32eb5ffebab522a89018cd
tushar-behera15 <beheratushar013@gmail.com>
2026-01-28 00:05:13 +0530

    Refactor validation schemas; update error messages for clarity and consistency in register and login schemas

--- src/validators/auth.schema.ts
+++ src/validators/auth.schema.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-    name: z.string().min(2, "Name too short"),
-    email: z.string().email("Invalid email"),
-    password: z.string().min(6, "Password must be at least 6 chars")
+    name: z.string({ error: "Name is required" }).min(2, "Name too short"),
+    email: z.email("A valid email is reqired"),
+    password: z.string("Password is required").min(6, "Password must be at least 6 chars")
 });
 
 export const loginSchema = z.object({
-    email: z.string().email("Invalid email"),
-    password: z.string().min(6)
+    email: z.email("A valid email is reqired"),
+    password: z.string("Password is required").min(6)
 });
